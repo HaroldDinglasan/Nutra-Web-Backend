06313f115b0d44505ba2b604e0dac442908dbb77
unknown <dinglasan.harold.ian.dave@gmail.com>
2025-03-20 16:06:56 +0800

    Add Register User

--- connectionHelper/db.js
+++ connectionHelper/db.js
@@ -1,27 +1,54 @@
 const sql = require("mssql");
 require("dotenv").config();  // Loads environment variables from .env file
 
-// Database connection configuration
-const config = {
-  user: process.env.DB_USER,          
-  password: process.env.DB_PASSWORD,  
-  server: process.env.DB_SERVER,      
-  database: process.env.DB_NAME,     
+
+// Database conifguration for PurchaseRequestDB (Registration)
+const purchaseRequestConfig = {
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  server:process.env.DB_SERVER,
+  database:process.env.DB_NAME_PURCHASE,
   options: {
-    encrypt: false,                   // Set to true if using Azure
-    trustServerCertificate: true,      // Required for self-signed certificates
+    encrypt: false, // Set to true if using Azure
+    trustServerCertificate: true,
   },
 };
 
-// Create a connection pool
-const poolPromise = new sql.ConnectionPool(config)
+// Database configuration for AVLI (Employee Names)
+const avliConfig = {
+  user: process.env.DB_USER,          
+  password: process.env.DB_PASSWORD,  
+  server: process.env.DB_SERVER,      
+  database: process.env.DB_NAME_AVLI,   
+  options: {
+    encrypt: false,                  
+    trustServerCertificate: true, 
+  },
+};
+
+
+const poolPurchaseRequest = new sql.ConnectionPool(purchaseRequestConfig)
   .connect()
   .then((pool) => {
-    console.log("✅ Connected to SQL Server");
+    console.log("✅ Connected to PurchaseRequestDB");
     return pool;
   })
   .catch((err) => {
-    console.error("❌ Database Connection Failed!", err);
+    console.error("❌ PurchaseRequestDB Connection Failed!", err);
   });
 
-module.exports = { sql, poolPromise };
+
+
+
+  const poolAVLI = new sql.ConnectionPool(avliConfig)
+  .connect()
+  .then((pool) => {
+    console.log("✅ Connected to AVLI");
+    return pool;
+  })
+  .catch((err) => {
+    console.error("❌ AVLI Connection Failed!", err);
+  });
+
+
+module.exports = { sql, poolPurchaseRequest, poolAVLI };
--- model/employeeModel.js
+++ model/employeeModel.js
@@ -1,10 +1,10 @@
 // Fetches Full name from AVLI SecuritySystemUser table
-const { sql, poolPromise } = require("../connectionHelper/db");
+const { sql, poolAVLI } = require("../connectionHelper/db");
 
 // Get Employee Full Names from AVLI Database
 const getEmployees = async () => {
   try {
-    const pool = await poolPromise;
+    const pool = await poolAVLI;
     const result = await pool
       .request()
       .query("SELECT FullName FROM AVLI.dbo.SecuritySystemUser"); // Cross-database query
