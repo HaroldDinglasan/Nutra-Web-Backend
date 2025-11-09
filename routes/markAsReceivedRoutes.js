ba124002feb7fe0d8c88148b01d4b2b8128f44e1
Harold Dinglasan <dinglasan.harold.ian.dave@gmail.com>
2025-11-09 15:48:51 +0800

    Fixed Remarks and DateDelivered admin

--- routes/markAsReceivedRoutes.js
+++ routes/markAsReceivedRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express")
-const { markAsReceived, getDeliveredList } = require("../controller/markAsReceivedController")
+const { markAsReceived, getDeliveredList, getRemarksById, updateRemarks } = require("../controller/markAsReceivedController")
 
 const router = express.Router()
 
 router.put("/markAsReceived/:Id", markAsReceived) // route to update mark as received per stock item
 router.get("/getDeliveredList", getDeliveredList) // route to get list of delivered stock items
-
+router.put("/updateRemarks/:Id", updateRemarks)   // route to update remarks
+router.get("/getRemarks/:Id", getRemarksById ) // route para mag display ang remarks
 module.exports = router
